d3a7bea86e4ca2c061cd572b1c27c840a7e143c1
tayaracaires <tayaracaires@gmail.com>
2019-09-13 13:43:30 -0300

    funcao busca pokemon

--- src/data.js
+++ src/data.js
@@ -1,8 +1,52 @@
 // esta é uma função de exemplo
 // veja como agregamos a função ao objeto global window
 
-const example = () => {
+/*const example = () => {
   return "example";
 };
 
-window.example = example;
+window.example = example;*/
+
+
+const POKEMONS = POKEMON.pokemon;
+
+for (let poke of POKEMONS){
+  document.write(`${poke.name}<br>`);
+  var name = document.getElementById("test");
+  name.innerHTML += '<name = "${poke.name}">';
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
